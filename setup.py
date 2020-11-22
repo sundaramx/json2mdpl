577c0d08a856338b85df7151daffb0b0c6176c2a
sundaramx <sundaramx@gmail.com>
2020-11-22 13:27:15 -0600

    updated structure

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="json2mdpl",
-    version=str(),
+    version="1.0.0",
     author="Sundar Rajan",
     description="json2mdpl is a python library for converting json to md ",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/sundaramx/json2mdpl",
     packages=setuptools.find_packages(),
     entry_points={
         "scripts": [
@@ -18,7 +19,7 @@ setuptools.setup(
     },
     classifiers=[
         "Language :: Python :: 3.x",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
     ],
     python_requires='>=3.x',
 )
--- tests/test_json2mdpl.py
+++ tests/test_json2mdpl.py
@@ -5,7 +5,7 @@ from json2mdpl import convertJson2md
 
 
 def test_convert():
-    infile = os.path.join(os.path.dirname(__file__), '.', 'reports', 'sample.json')
+    infile = os.path.join(os.path.dirname(__file__), '..', 'reports', 'sample.json')
     with tempfile.NamedTemporaryFile() as outfile:
         convertJson2md(infile, './reports/sample.md')
 
